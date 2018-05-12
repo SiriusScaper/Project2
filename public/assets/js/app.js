496e545518088fb6150264eb6607cda10caf4d93
SiriusScaper <siriusscaper@gmail.com>
2018-05-12 11:19:15 -0700

    Add logic for getting user location

---
+++ public/assets/js/app.js
@@ -0,0 +1,42 @@
+//Require node-geocoder
+const NodeGeocoder = require('node-geocoder');
+
+// Call to navigator API for user location
+function currLocation(position) {
+	const lat = position.coords.latitude;
+	const lng = poistion.coords.longitude;
+
+	let currentLocation = [lat, lng];
+
+	const gogleLoc = new google.maps.LatLng(
+		position.coords.latitude,
+		position.coords.longitude
+	);
+	locations.push(googleLoc);
+
+	console.log(currentLocation);
+}
+
+// Handle Errors
+const displayError = error => {
+	const errors = [
+		'Unknown error',
+		'Permission denied by user',
+		'Poistion is not available',
+		'Timeout error'
+	];
+	const message = errors[error.code];
+	console.warn(`Error in getting your location: ${message}, ${error.message}`);
+};
+
+//
+
+window.onload = () => {};
+
+window.onload = function() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+	} else {
+		alert("Sorry, this browser doesn't support geolocation!");
+	}
+};
